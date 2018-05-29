d4ece2c78d6ee8627ba640b8ed8ec680c0d77e29
YFWCQ <1049155621@qq.com>
2018-05-29 15:22:59 +0800

    1

--- YFFighterTeris/Tetris/TetrisView/Model/YFTerisBaModel.swift
+++ YFFighterTeris/Tetris/TetrisView/Model/YFTerisBaModel.swift
@@ -67,9 +67,9 @@ class YFTerisBaModel: NSObject {
     }
     
     
-    func checkIsCan(<#parameters#>) -> <#return type#> {
-        <#function body#>
-    }
-    
+//    func checkIsCan(<#parameters#>) -> <#return type#> {
+//        <#function body#>
+//    }
+//
     
 }
